51598f0926ff12d4c4a3bd114043dc444c751e3c
Hirbod Behnam <chrome.hiri.angry@gmail.com>
2023-09-13 16:17:30 +0330

    Added done status

--- coroutine.c
+++ coroutine.c
@@ -3,35 +3,43 @@
 #include "coroutine.h"
 #include "coroutine_offsets.h"
 
-/**
- * Gets the initial top of stack pointer of a coroutine
- * @param c The coroutine to get its top of stack
- * @return The RSP
- */
-static uint64_t initial_top_of_stack(struct coroutine *c) {
-    // Remember that stack grows from top to bottom.
-    // First 8 bytes are function address
-    uint64_t stack_pointer = (uint64_t) &(c->stack[c->stack_size - sizeof(uint64_t)]);
-    // Read more: https://stackoverflow.com/a/51072200/4213397
-    while (stack_pointer % 16 != 0)
-        stack_pointer--;
-    return stack_pointer;
-}
-
-/**
- * Extracts the address of a function and puts it on top of stack.
- * @param f The function to extract its address
- * @param stack Top of stack
- */
-static void extract_function_address(void (f)(struct coroutine *, void *), uint64_t *stack) {
-    stack[0] = (uint64_t) f;
-}
-
 // Assembly functions
 
 extern void coroutine_start_inner(struct coroutine *c, void *);
 
 extern void coroutine_continue_inner(struct coroutine *c);
+
+extern void coroutine_finish(void);
+
+/**
+ * Gets the initial top of stack pointer of a coroutine
+ * @param c The coroutine to get its top of stack
+ * @return The RSP
+ */
+static void initial_top_of_stack(struct coroutine *c) {
+    // Remember that stack grows from top to bottom.
+    // First 8 bytes are function address.
+    // Next 8 bytes is the address to coroutine_finish method in switch.S
+    // Last 8 bytes is the address of the coroutine itself.
+    uint64_t stack_pointer = (uint64_t) &(c->stack[c->stack_size - 3 * 8]);
+    // Read more: https://stackoverflow.com/a/51072200/4213397
+    while (stack_pointer % 16 != 0)
+        stack_pointer--;
+    c->stack_pointer = stack_pointer;
+}
+
+/**
+ * Setups the stack of the a coroutine
+ * @param c The coroutine
+ * @param f The initial function to call
+ */
+static void setup_stack(struct coroutine *c, void (f)(struct coroutine *, void *)) {
+    initial_top_of_stack(c);
+    uint64_t *stack = (uint64_t *) (c->stack_pointer);
+    stack[0] = (uint64_t) f; // first ret will redirect us to this function
+    stack[1] = (uint64_t) coroutine_finish; // after the given function is done this subroutine will be called
+    stack[2] = (uint64_t) c; // pointer to coroutine
+}
 
 int coroutine_create(struct coroutine *c, size_t stack_size) {
     // Assert to don't fuck up in start_task_inner
@@ -39,6 +47,7 @@ int coroutine_create(struct coroutine *c, size_t stack_size) {
                    "return_stack_pointer");
     _Static_assert(offsetof(struct coroutine, stack_pointer) == COROUTINE_STACK_POINTER_OFFSET, "stack_pointer");
     _Static_assert(offsetof(struct coroutine, status) == COROUTINE_STATUS_OFFSET, "status");
+    _Static_assert(COROUTINE_STATUS_DONE_VALUE == STATUS_DONE, "status value");
     // Allocate stack
     char *stack = malloc(stack_size);
     if (stack == NULL)
@@ -46,8 +55,7 @@ int coroutine_create(struct coroutine *c, size_t stack_size) {
     // Set result
     c->stack = stack;
     c->stack_size = stack_size;
-    c->stack_pointer = initial_top_of_stack(c);
-    assert(c->stack_pointer % 16 == 0);
+    c->stack_pointer = 0; // better be safe than sorry
     c->return_stack_pointer = 0; // not needed but whatever
     c->status = STATUS_CREATED;
     return 0;
@@ -58,7 +66,8 @@ int coroutine_start(struct coroutine *c, void (f)(struct coroutine *, void *), v
     if (c->status != STATUS_CREATED)
         return 1;
     // Extract the address of function to execute
-    extract_function_address(f, (uint64_t *) (c->stack_pointer));
+    setup_stack(c, f);
+    assert(c->stack_pointer % 16 == 0);
     // Start it!
     c->status = STATUS_RUNNING;
     coroutine_start_inner(c, argument);
--- main.c
+++ main.c
@@ -8,7 +8,9 @@ void counter(struct coroutine *this, void *arg) {
         printf("counter %d\n", i);
         coroutine_yield(this);
     }
+    printf("Yielding one last time!\n");
     coroutine_yield(this);
+    puts("Exiting...");
 }
 
 // Main functions
@@ -18,7 +20,7 @@ int main() {
     coroutine_create(&my_coroutine, 1024 * 1024);
     // Jump to it!
     coroutine_start(&my_coroutine, counter, NULL);
-    for (int i = 0; i < 5; i++)
+    while (my_coroutine.status != STATUS_DONE)
         coroutine_continue(&my_coroutine);
     printf("Done!\n");
     coroutine_free(&my_coroutine);
