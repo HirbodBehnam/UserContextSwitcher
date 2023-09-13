18ee1c10a7e1f10558ffa16297a2d03fd4754cb5
Hirbod Behnam <chrome.hiri.angry@gmail.com>
2023-09-13 08:54:36 +0330

    Partially working. Need to save the base pointer

--- main.c
+++ main.c
@@ -3,12 +3,6 @@
 #include <stdio.h>
 #include <stdint.h>
 
-void assert_stack() {
-    uint64_t rsp;
-    asm volatile ("mov %0, rsp" : "=r" (rsp));
-    assert(rsp % 16 == 0);
-}
-
 // Coroutines
 #define COROUTINE_STACK_SIZE (1024 * 1024)
 
@@ -18,24 +12,19 @@ struct coroutine {
     char stack[COROUTINE_STACK_SIZE];
 };
 
-// Assembly functions
-extern void switch_to(struct coroutine *c);
-
-extern void yield(struct coroutine *this);
-
-// Program coroutines
-struct coroutine my_coroutine;
-
-void counter(void) {
-    for (int i = 0; i < 10; i++) {
-        //puts("Kir?");
-        printf("counter %d\n", i);
-        yield(&my_coroutine);
-    }
-    yield(&my_coroutine);
+/**
+ * Gets the initial top of stack pointer of a coroutine
+ * @param c The coroutine to get its top of stack
+ * @return The RSP
+ */
+uint64_t initial_top_of_stack(struct coroutine *c) {
+    uint64_t stack_pointer = (uint64_t) &(c->stack[COROUTINE_STACK_SIZE - 8]);
+    // Read more: https://stackoverflow.com/a/51072200/4213397
+    while (stack_pointer % 16 != 0)
+        stack_pointer--;
+    return stack_pointer;
 }
 
-// Main functions
 void extract_function_address(void (f)(void), char *stack) {
     const uint64_t function_address = (uint64_t) f;
     const uint32_t low = function_address & UINT32_MAX, high = (function_address >> 32) & UINT32_MAX;
@@ -44,19 +33,39 @@ void extract_function_address(void (f)(void), char *stack) {
     casted_stack[1] = high;
 }
 
+// Assembly functions
+extern void start_task(struct coroutine *c);
+
+extern void switch_to_task(struct coroutine *c);
+
+extern void yield_task(struct coroutine *this);
+
+// Program coroutines
+struct coroutine my_coroutine;
+
+void counter(void) {
+    for (int i = 0; i < 10; i++) {
+        printf("counter %d\n", i);
+        yield_task(&my_coroutine);
+    }
+    yield_task(&my_coroutine);
+}
+
+// Main functions
 int main() {
-    assert_stack();
     _Static_assert(offsetof(struct coroutine, return_stack_pointer) == 0, "return_stack_pointer");
     _Static_assert(offsetof(struct coroutine, stack_pointer) == 8, "stack_pointer");
     _Static_assert(offsetof(struct coroutine, stack) == 16, "stack");
     printf("Hello, World!\n");
     // Remember that stack grows from top to bottom.
     // First 8 bytes are function address
-    my_coroutine.stack_pointer = (uint64_t) &my_coroutine.stack[COROUTINE_STACK_SIZE - 8];
-    assert((my_coroutine.stack_pointer + 8) % 16 == 0);
-    extract_function_address(&counter, &my_coroutine.stack[COROUTINE_STACK_SIZE - 8]);
+    my_coroutine.stack_pointer = initial_top_of_stack(&my_coroutine);
+    assert(my_coroutine.stack_pointer % 16 == 0);
+    extract_function_address(&counter, (char *) (my_coroutine.stack_pointer));
     // Jump to it!
-    switch_to(&my_coroutine);
+    start_task(&my_coroutine);
+    for (int i = 0; i < 5; i++)
+        switch_to_task(&my_coroutine);
     printf("Done!\n");
     return 0;
 }
--- switch.s
+++ switch.s
@@ -2,9 +2,9 @@
 .text
 .align 8
 .intel_syntax noprefix
-.globl switch_to
-.type switch_to, @function
-switch_to:
+.globl start_task
+.type start_task, @function
+start_task:
 	# Save all registers
 	push rax
 	push rbx
@@ -15,9 +15,33 @@ switch_to:
 	mov rsp, [rdi+8] # Load coroutine stack pointer
 	ret # This return will make a jump to coroutine
 
-.globl yield
-.type yield, @function
-yield:
+.globl switch_to_task
+.type switch_to_task, @function
+switch_to_task:
+	# Save all registers
+	push rax
+	push rbx
+	push rcx
+	push rdx
+	# Swap stack pointers
+	mov [rdi], rsp # Save main stack pointer
+	mov rsp, [rdi+8] # Load coroutine stack pointer
+	# Restore registers
+	pop rdx
+    pop rcx
+    pop rbx
+    pop rax
+	ret # This return will make a jump to coroutine
+
+.globl yield_task
+.type yield_task, @function
+yield_task:
+	# Store task registers
+	push rax
+    push rbx
+    push rcx
+    push rdx
+    # Swap stack pointers
 	mov [rdi+8], rsp # Save coroutine stack pointer
 	mov rsp, [rdi] # Load main stack pointer
 	# Restore registers
